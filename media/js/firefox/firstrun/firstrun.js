2f4ac27fec8ef0f80c1b0c55aa6dd9e15c319230
gasolin <gasolin@gmail.com>
2017-09-27 17:47:50 +0800

    Bug 1386184 - Firstrun page should skip the FxA form if the user is signed in

--- media/js/firefox/firstrun/firstrun.js
+++ media/js/firefox/firstrun/firstrun.js
@@ -7,58 +7,73 @@
 
     var animateSunrise = function () {
         var scene = document.getElementById('scene');
-        var skipbutton = document.getElementById('skip-button');
 
-        var hideOrShowSkipButton = function (data) {
-            switch(data.data.url) {
-            case 'signin':
-            case 'signup':
-            case 'reset_password':
-                skipbutton.disabled = false;
-                skipbutton.classList.remove('skipbutton-hidden');
-                break;
-            default:
-                skipbutton.classList.add('skipbutton-hidden');
-                break;
+        var redirect = function(event) {
+            document.getElementById('sunrise').removeEventListener('transitionend', redirect);
+            if (event.propertyName === 'transform') {
+                window.setTimeout(function () {
+                    // Bug 1381051 sign in should redirect to about:home instead of about:newtab.
+                    window.location.href = 'about:home';
+                }, 200);
             }
-        };
-
-        var disableSkipButton = function () {
-            skipbutton.disabled = true;
         };
 
         var onVerificationComplete = function () {
             scene.dataset.signIn = 'true';
-            document.getElementById('sunrise').addEventListener('transitionend', function(event) {
-                if (event.propertyName === 'transform') {
-                    window.setTimeout(function () {
-                        // Bug 1381051 sign in should redirect to about:home instead of about:newtab.
-                        window.location.href = 'about:home';
-                    }, 200);
-                }
-            }, false);
+            document.getElementById('sunrise').addEventListener('transitionend', redirect, false);
         };
 
-        skipbutton.onclick = onVerificationComplete;
+        Mozilla.UITour.getConfiguration('sync', function(config) {
+            if (config.setup) {
+                // skip showing the sign in form if the user is signed in
+                document.getElementById('sky').addEventListener('transitionend', function(event) {
+                    if (event.propertyName === 'opacity') {
+                        scene.dataset.skipStep = 'true';
+                        onVerificationComplete();
+                    }
+                }, false);
+            } else {
+                document.getElementById('sky').addEventListener('transitionend', function(event) {
+                    if (event.propertyName === 'opacity') {
+                        scene.dataset.modal = 'true';
+                    }
+                }, false);
 
-        Mozilla.Client.getFirefoxDetails(function(data) {
-            Mozilla.FxaIframe.init({
-                distribution: data.distribution,
-                gaEventName: 'firstrun-fxa',
-                onVerificationComplete: onVerificationComplete,
-                onLogin: onVerificationComplete,
-                onFormEnabled: disableSkipButton,
-                onNavigated:  hideOrShowSkipButton
-            });
+                var skipbutton = document.getElementById('skip-button');
+                skipbutton.onclick = onVerificationComplete;
+                
+                var hideOrShowSkipButton = function (data) {
+                    switch(data.data.url) {
+                    case 'signin':
+                    case 'signup':
+                    case 'reset_password':
+                        skipbutton.disabled = false;
+                        skipbutton.classList.remove('skipbutton-hidden');
+                        break;
+                    default:
+                        skipbutton.classList.add('skipbutton-hidden');
+                        break;
+                    }
+                };
+        
+                var disableSkipButton = function () {
+                    skipbutton.disabled = true;
+                };
+
+                Mozilla.Client.getFirefoxDetails(function(data) {
+                    Mozilla.FxaIframe.init({
+                        distribution: data.distribution,
+                        gaEventName: 'firstrun-fxa',
+                        onVerificationComplete: onVerificationComplete,
+                        onLogin: onVerificationComplete,
+                        onFormEnabled: disableSkipButton,
+                        onNavigated:  hideOrShowSkipButton
+                    });
+                });
+            }
         });
 
         scene.dataset.sunrise = 'true';
-
-        document.getElementById('sky').addEventListener('transitionend', function(event) {
-            if (event.propertyName === 'opacity') {
-                scene.dataset.modal = 'true';
-            }
-        }, false);
     };
 
     document.onreadystatechange = function () {
